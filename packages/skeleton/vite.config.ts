3a803ea601512410b5173573120a8f652448c742
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-05-13 16:03:05 -0400

    dont need that

--- packages/skeleton/vite.config.ts
+++ packages/skeleton/vite.config.ts
@@ -1,19 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import { configDefaults } from 'vitest/config';
-import sveld from 'vite-plugin-sveld';
-
-// Import package.json version
-import { readFileSync } from 'fs';
-const json = readFileSync('package.json', 'utf8');
-const pkg = JSON.parse(json);
 
 /** @type {import('vite').UserConfig} */
 const config: UserConfig = {
-	plugins: [sveltekit(), sveld()],
-	define: {
-		__PACKAGE__: pkg
-	},
+	plugins: [sveltekit()],
 	test: {
 		globals: true,
 		environment: 'jsdom',
